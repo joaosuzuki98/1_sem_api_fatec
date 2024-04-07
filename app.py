e1bb0b2d1a79cca3eba2b377a4a4ecba7215bbce
João Suzuki <joaosuzuki98@outlook.com>
2024-04-07 00:27:42 -0300

    Criação da animação de foco para o timelapse

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app = Flask("__name__")
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/show-data")
+def show_data():
+    return render_template('show_data.html')
--- static/js/timelapse.js
+++ static/js/timelapse.js
@@ -1,3 +1,4 @@
+// Objeto para configuração do swiper
 const swiper = new Swiper(".swiper", {
     effect: "coverflow",
     coverflowEffect: {
@@ -10,4 +11,22 @@ const swiper = new Swiper(".swiper", {
     },
     slidesPerView: 3,
     spaceBetween: 30,
-  });
+});
+
+const swiperEle = document.querySelector('.swiper').swiper;
+
+// Para que o slide atual fique focado quando a página for carregada
+swiperEle.slides[swiperEle.activeIndex + 1].querySelector('.day-shape').style.backgroundColor = '#337357';
+
+// Criação do efeito de focus no slide atual e do link
+swiperEle.on('slideChange', function () {
+    const activeSlide = this.activeIndex;
+    const slides = this.slides;
+    for (var i = 0; i < slides.length; i++) {
+        var sliderShape = slides[i].querySelector('.day-shape');
+        sliderShape.style.backgroundColor = '#4F9476';
+    }
+    const activeSliderShape =  slides[activeSlide + 1].querySelector('.day-shape')
+    activeSliderShape.style.transition = 'background-color 500ms';
+    activeSliderShape.style.backgroundColor = '#337357';
+});
