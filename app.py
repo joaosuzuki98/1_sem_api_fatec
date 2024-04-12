37fe5dddcd7180714a7a2b93351b6a7fda67fc34
jfiliprc <felipvsrochaconst@gmail.com>
2024-04-12 14:01:04 -0300

    Integração e criação do banco de dados.

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 import os
 from openpyxl import load_workbook
 from werkzeug.utils import secure_filename
-
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
 ALLOWED_EXTENSIONS = {'xlsx'}
 
 
@@ -12,26 +12,33 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in \
         ALLOWED_EXTENSIONS
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    soil_humidity = db.Column(db.Float, nullable=False)
+    ambient_humidity = db.Column(db.Float, nullable=False)
+    ambient_temperature = db.Column(db.Float, nullable=False)
+    water_volume = db.Column(db.Float, nullable=False)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template('index.html')
 
 @app.route("/show-data")
 def show_data():
     return render_template('show_data.html')
 
-
 @app.route("/add-data")
 def add_data():
     return render_template('add_data.html')
 
-
 @app.route("/delete-data")
 def delete_data():
     return render_template('delete_data.html')
-
 
 @app.route("/statistics")
 def statistics():
