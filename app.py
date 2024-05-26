cdf2e606365993d7c90807433ef65b1c0b604b93
João Suzuki <joaosuzuki98@outlook.com>
2024-05-25 22:46:03 -0300

    Correção de um erro maluco que deu

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import os
 from openpyxl import load_workbook
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import distinct
+from filters import enumerate_filter
 import variables
-from datetime import datetime , timedelta
-from collections import defaultdict
+import datetime
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'xlsx'}
-hjdia='12'
-hjmes='09'
-hjano='2023'
+
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in \
@@ -39,16 +38,30 @@ class Code(db.Model):
 with app.app_context():
     db.create_all()
 
+# Registrando o filtro criado em filters.py
+app.jinja_env.filters['enumerate'] = enumerate_filter
+
 
 @app.route("/")
 def index():
     overview_list = zip(variables.svg_overview_list, variables.overview_desc)
-    
 
-    # Retornar os dados para o template do Chart.js
+    # Realizando um select * em nossa tabela e guardando estes valores em
+    # uma variável
+    dados_db = Data.query.all()
 
-    mes = datetime.now().strftime('%h')
-    return render_template('index.html', overview_list=overview_list, mes=mes)
+    # Aqui estou pegando todos os valores do campo date e os tornando único,
+    # pois há vários dias iguais gravados no banco de dados
+    datas = db.session.query(distinct(Data.date)).all()
+
+    datas_index = 0  # variável necessária para atribuir ao atributo key a data completa
+
+    return render_template(
+        'index.html',
+        overview_list=overview_list,
+        dados_db=dados_db,
+        datas=datas,
+        datas_index=datas_index)
 
 
 @app.route("/trocar", methods=["POST"])
@@ -75,13 +88,7 @@ def trocar():
 
 @app.route("/show-data")
 def show_data():
-    
-    data = request.get_json()
-    variable = data['variable']
-    # Do something with variable
-    
-    dados_por_dia = db.session.query(Data).filter_by(date=variable).all()
-    return jsonify({'status': 'success'}), 200
+    return render_template('show_data.html')
 
 
 @app.route("/add-data")
@@ -97,111 +104,21 @@ def delete_data():
 @app.route("/del-dia", methods=["POST"])
 def del_dia():
     senha = request.form['senha']
-    data_atual = request.form['deldia']
     with app.app_context():
         codigo = Code.query.first()
     if senha == codigo.password:
-        data_atual = datetime.strptime(data_atual, "%Y-%m-%d").date() 
-        deleted_data = Data.query.filter_by(date=data_atual).delete()
-        db.session.commit()
-        if deleted_data:
-            return render_template("delete_data.html", success="Dia deletado com sucesso.")
-        else:
-            return render_template("delete_data.html", error="Nenhum dado encontrado para a data fornecida.")
-    else:
-        return render_template("delete_data.html", error="Código de segurança errado.")
+        return render_template("delete_data.html", success="Dia deletado com sucesso.")
+    return render_template("delete_data.html", error="Código de segurança errado.")
 
- 
-@app.route("/statistics", methods=["GET", "POST"])
+
+@app.route("/statistics")
 def statistics():
-    lista = []
-    if request.method == "POST":
-        interval = request.form.get("interval")
-        if interval:
-            interval = int(interval)
-            end_date = datetime.now()
-            start_date = end_date - timedelta(days=interval)
-            lista = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
-        else:
-            date = str(request.form["data"]).split("-")
-            dia = int(date[2])
-            mes = int(date[1])
-            ano = int(date[0])
-            datef = str(request.form["dataf"]).split("-")
-            diaf = int(datef[2])
-            mesf = int(datef[1])
-            anof = int(datef[0])
-
-            start_date = datetime(ano, mes, dia)
-            end_date = datetime(anof, mesf, diaf)
-            
-            current_date = start_date
-            while current_date <= end_date:
-                lista.append(current_date.strftime('%Y-%m-%d'))
-                current_date += timedelta(days=1)
-        
-        print(lista)
-    
-    listasoilh = []
-    listaambienth = []
-    listaambientt = []
-    listawater = []
-    
-    for date_str in lista:
-        try:
-            dia = datetime.strptime(date_str, '%Y-%m-%d').date()
-        except ValueError:
-            return jsonify({"error": "Invalid date format. Use YYYY-MM-DD."}), 400
-    
-        dados_por_dia = db.session.query(Data).filter_by(date=dia).all()
-        if not dados_por_dia:
-            return jsonify({"message": f"No data found for {dia}."})
-
-        # Dicionário para armazenar somas e contagens por atributo
-        soma_por_atributo = defaultdict(float)
-        contagem_por_atributo = defaultdict(int)
-
-      # Calcular a soma e contagem para cada atributo
-        for data in dados_por_dia:  
-            soma_por_atributo["soil_humidity"] += round(data.soil_humidity, 2)
-            soma_por_atributo["ambient_humidity"] += round(data.ambient_humidity, 2)
-            soma_por_atributo["ambient_temperature"] += round(data.ambient_temperature, 2)
-            soma_por_atributo["water_volume"] += round(data.water_volume*1000, 2)
-            contagem_por_atributo["soil_humidity"] += 1
-            contagem_por_atributo["ambient_humidity"] += 1
-            contagem_por_atributo["ambient_temperature"] += 1
-            contagem_por_atributo["water_volume"] += 1
-                
-        # Calcular as médias
-        media_por_atributo = {atributo: round(soma_por_atributo[atributo] / contagem_por_atributo[atributo], 1) for atributo in soma_por_atributo}
-        listasoilh.append(media_por_atributo["soil_humidity"])
-        listaambienth.append(media_por_atributo["ambient_humidity"])
-        listaambientt.append(media_por_atributo["ambient_temperature"])
-        listawater.append(media_por_atributo["water_volume"])
-       
-    print("Lista Soil Humidity:", listasoilh)
-    print("Lista Ambient Humidity:", listaambienth)
-    print("Lista Ambient Temperature:", listaambientt)
-    print("Lista Water Volume:", listawater)
-    print(lista)
-   
-
-    # Agora você tem um dicionário onde as chaves são as datas e os valores são dicionários contendo as médias de cada atributo para cada dia
-
-    # Exibir o dicionário (opcional)
-    
-
-    return render_template('statistics.html', 
-                           listasoilh=listasoilh,
-                           listaambienth=listaambienth,
-                           listaambientt=listaambientt,
-                           listawater=listawater,  lista=lista)
+    return render_template('statistics.html')
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
     if request.method == "POST":
-        table_html = "<table>"
         file = request.files["file"]
         if not os.path.exists('./uploads'):
             os.mkdir('./uploads')
@@ -216,7 +133,7 @@ def upload():
             for row in range(1, sheet.max_row + 1):
                 try:
                     info = Data()
-                    info.date = datetime.strptime(str(sheet.cell(row=row, column=2).value).split(" ")[0], "%Y-%m-%d")
+                    info.date = datetime.datetime.strptime(str(sheet.cell(row=row, column=2).value).split(" ")[0], "%Y-%m-%d")
                     info.time = sheet.cell(row=row, column=3).value
                     info.soil_humidity = sheet.cell(row=row, column=4).value
                     info.ambient_humidity = sheet.cell(row=row, column=5).value
@@ -229,5 +146,6 @@ def upload():
                         cell = sheet.cell(row=row, column=col)
                         table_html += f"<td style='color: black'>{cell.value}</td>"
                     table_html += "</tr>"
-        db.session.commit()
-        return render_template("add_data.html", table_html=table_html)
+            db.session.commit()
+            return render_template("add_data.html", table_html=table_html)
+
--- static/js/grafico.js
+++ static/js/grafico.js
@@ -1,5 +1,137 @@
+// Dados
+const labels = ['27/01', '28/01', '29/01', '30/01', '31/01']; // Labels de meses
+const umidade = [65, 68, 70, 72, 68]; // Umidade
+const umidadeSolo = [45, 42, 40, 38, 42]; // Umidade do solo
+const volumeAgua = [100, 95, 90, 85, 90]; // Volume da água
+const temperaturaAmbiente = [25, 26, 27, 28, 26]; // Temperatura ambiente
 
- 
+Chart.defaults.color = '#efefef';
+
+// Renderizar o gráfico
+var myChart = new Chart(
+    document.getElementById('myChart'), {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Umidade (%)',
+                data: umidade,
+                borderColor: 'blue',
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: '#292929'
+                    }
+                },
+                x: {
+                    grid: {
+                        color: '#292929'
+                    }
+                }
+            }
+        }
+    });
+
+var anotherChart = new Chart(
+    document.getElementById('anotherChart'), {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Umidade do Solo (%)',
+                data: umidadeSolo,
+                borderColor: 'green',
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: '#292929'
+                    }
+                },
+                x: {
+                    grid: {
+                        color: '#292929'
+                    }
+                }
+            }
+        }
+    });
+
+var evenAnotherChart = new Chart(
+    document.getElementById('evenAnotherChart'), {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Volume da Água (Litros)',
+                data: volumeAgua,
+                borderColor: 'orange',
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: '#292929'
+                    }
+                },
+                x: {
+                    grid: {
+                        color: '#292929'
+                    }
+                }
+            }
+        }
+    });
+
+var oneLastChart = new Chart(
+    document.getElementById('oneLastChart'), {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Temperatura Ambiente (°C)',
+                data: temperaturaAmbiente,
+                borderColor: 'purple',
+                fill: false,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: '#292929'
+                    }
+                },
+                x: {
+                    grid: {
+                        color: '#292929'
+                    }
+                }
+            }
+        }
+    });
+
 
 const downloadBtn = document.getElementById('download-btn');
 
